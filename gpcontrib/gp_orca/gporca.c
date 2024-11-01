c33ade8b134fcf9ae3e40d5207afa9c28640a9bd
Roman Eskin <r.eskin@arenadata.io>
2024-11-01 17:43:18 +1000

    Add gp_orca extension skeleton

---
+++ gpcontrib/gp_orca/gporca.c
@@ -0,0 +1,19 @@
+#include "postgres.h"
+
+#include "utils/builtins.h"
+
+PG_MODULE_MAGIC;
+
+void		_PG_init(void);
+void		_PG_fini(void);
+
+void
+_PG_init(void)
+{
+}
+
+void
+_PG_fini(void)
+{
+}
+
