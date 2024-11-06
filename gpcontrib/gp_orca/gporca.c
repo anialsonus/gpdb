e506e6a5a1968e91dcb898cee6d8ce8d9dbb0363
Roman Eskin <r.eskin@arenadata.io>
2024-11-06 17:58:59 +1000

    Update _PG_init() in gp_orca

--- gpcontrib/gp_orca/gporca.c
+++ gpcontrib/gp_orca/gporca.c
@@ -1,5 +1,7 @@
 #include "postgres.h"
 
+#include "cdb/cdbvars.h"
+#include "miscadmin.h"
 #include "utils/builtins.h"
 
 PG_MODULE_MAGIC;
@@ -10,6 +12,13 @@ void		_PG_fini(void);
 void
 _PG_init(void)
 {
+	if (!process_shared_preload_libraries_in_progress)
+		ereport(ERROR,
+				(errcode(ERRCODE_INTERNAL_ERROR),
+				 errmsg("This module can only be loaded via shared_preload_libraries")));
+
+	if (!IS_QUERY_DISPATCHER())
+		return;
 }
 
 void
